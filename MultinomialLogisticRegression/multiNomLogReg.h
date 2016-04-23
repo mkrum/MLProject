cf7733cee3fe701c1e8b4ad03f47fd57472c91e2
root <root@localhost.localdomain>
2016-04-23 15:47:29 -0400

    Developing multiNomLogReg.h

---
+++ MultinomialLogisticRegression/multiNomLogReg.h
@@ -0,0 +1,15 @@
+//Class to execute Multinomial Logistic Regression on a dataset
+//Will Kudela
+
+#include "dataset.h"
+#include <iostream>
+#include <string>
+
+class multiNomLogReg
+{
+	public:
+		
+	private:
+		int m; //number of explanatory variables
+		int k; //number of categories
+};
