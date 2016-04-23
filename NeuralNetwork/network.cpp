c473325366554d324ec8d71facf90106678d2607
Luke William Duane <lduane@nd.edu>
2016-04-23 16:32:24 -0400

    test fixe

---
+++ NeuralNetwork/network.cpp
@@ -0,0 +1,25 @@
+//network.cpp
+//Luke Duane
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <time.h>
+#include "neuron.h"
+using namespace std;
+
+NeuralNetwork::NeuralNetwork(){
+    
+}
+
+void NeuralNetwork::train(){
+
+}
+
+void NeuralNetwork::backPropogate(){
+
+}
+
+
+
+
