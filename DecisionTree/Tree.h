58f50a47bc5d1d59f00302387e03ebc4969adb25
John Marco Bruscia <jbruscia@student01.cse.nd.edu>
2016-04-26 21:34:37 -0400

    Cleaned up code and removed no longer used files

--- DecisionTree/Tree.h
+++ DecisionTree/Tree.h
@@ -17,29 +17,18 @@ class Tree {
 		Tree();
 		~Tree();
 		TreeNode<double> getFirst();
-		void buildNode(vector<double>, TreeNode<double> &, int);
-		int getNumLevels();
-		TreeNode<double>  findNextNode(TreeNode<double> &);
-		int leftNode(TreeNode<double> &);
 		void makeTree(int, TreeNode<double> &, int);
 		void setLevel(int, TreeNode<double> &, double,int);
 		void setFirst (TreeNode<double> a){first = a;}
 	private:
-		vector < TreeNode<double> > tree;
 		TreeNode<double> first;
-		int numLevels;
 
 };
 
 
-Tree::Tree()
-{
-	
-}
+Tree::Tree(){}
 
-Tree::~Tree(){
-
-}
+Tree::~Tree(){}
 
 void Tree::makeTree(int numLevels,TreeNode<double> &k, int firstTime)
 {
@@ -55,7 +44,6 @@ void Tree::makeTree(int numLevels,TreeNode<double> &k, int firstTime)
 		makeTree(numLevels-1,*newNodeL,0);
 		makeTree(numLevels-1,*newNodeR,0);
 	}
-		//cout << "done" << endl;
 	if(firstTime == 1) first = k;
 }
 
@@ -64,7 +52,6 @@ void Tree::setLevel(int levelNum, TreeNode<double> &k, double average, int first
 	if(firstTime == 1) k=first;
 	if (levelNum > 1)
 	{
-		cout << "top" << endl;
 		setLevel(levelNum-1, *(k.getLeftChild()),average,0);
 		setLevel(levelNum-1, *(k.getRightChild()),average,0);
 	}
@@ -81,52 +68,4 @@ TreeNode<double> Tree::getFirst()
 	return first;
 }
 
-
-int Tree::getNumLevels() { return numLevels; }
-/*
-void Tree::buildNode(vector<double> a, TreeNode<double> &k, int count)
-{
-	cout << "in here famo" << endl;
-	TreeNode<double> c = getFirst();
-	TreeNode<double> n = findNextNode(c); //finds next available empty node
-	cout << "no way it gets here" << endl;
-	TreeNode<double> nextOne;
-	nextOne.setParent(n);
-	double average, sum=0;
-	for(int i=0;i<a.size();i++)
-		sum += a[i];
-	average=sum/a.size();
-	nextOne.setData(average);
-	cout << average << endl;
-	
-	
-	
-	if(count == 0)
-		k.setData(average);
-	else 
-	{	
-		buildNode(a,*(k->getLeftChild()),count-1);
-		buildNode(a,*(k->getRightChild()),count-1);
-	}	
-		
-	if(leftNode(n))
-		n.setLeft(nextOne);
-	else
-		n.setRight(nextOne);
-}
-
-TreeNode<double> Tree::findNextNode(TreeNode<double>  &a)//needs to be fixed
-{
-	if(a.getLeftChild() == NULL) return *(a.getLeftChild()->getParent());
-	if(a.getRightChild() == NULL) return *(a.getLeftChild()->getParent());
-	return findNextNode(*(a.getLeftChild()));
-}
-
-int Tree::leftNode(TreeNode<double> &n)
-{
-	if (n.getParent()->getLeftChild()->getData() ==  n.getData())
-		return 1;
-	else return 0;
-}
-*/
 #endif
--- DecisionTree/TreeNode.h
+++ DecisionTree/TreeNode.h
@@ -10,25 +10,17 @@
 #include <locale>
 using namespace std;
 
-//template<typename T> class LinkedList; // forward declaration of List
-
 template<typename T>   // templated node type
 class TreeNode {
 
-  
-
  public:
-  TreeNode(T, string);     // constructor
   TreeNode();
   ~TreeNode();
-  TreeNode(TreeNode<T> &, TreeNode<T> &);
   T getData() const;   // get function
-  string getLabel();
   TreeNode<T> * getLeftChild();
   TreeNode<T> * getRightChild();
   TreeNode<T> * getParent();
   map <string,double> getMap(){return numOfResults;}
-  void setLabel(string s){label = s;}
   void setData(double a){data = a;}
   string getMostPopularResult();
   void setParent(TreeNode<double> a){*parent = a;}
@@ -38,55 +30,30 @@ class TreeNode {
 
  private:
   T data;           // data for linked list node 
-  string label;
   map <string,double> numOfResults;
   TreeNode<T> *nextl;    // pointers to next object
   TreeNode<T> *nextr;
   TreeNode<T> *parent;
 };
 
-// NOTE:  all of the functions are in the .h file to overcome issues with
-//        templates and linking
-
-// constructor, using member initialization list syntax
 template<typename T>
-TreeNode<T>::~TreeNode(){
-}
+TreeNode<T>::TreeNode() : data(0), nextl(NULL),nextr(NULL){}
 
 template<typename T>
-TreeNode<T>::TreeNode(T a,string s) :data(a),nextl(NULL),nextr(NULL),label(s){}
+TreeNode<T>::~TreeNode(){}
 
-template<typename T>
-TreeNode<T>::TreeNode() : data(0), nextl(NULL),nextr(NULL),label(""){}
-
-template<typename T>
-TreeNode<T>::TreeNode(TreeNode<T> &a, TreeNode<T> &b) :data(0),nextl(&a),nextr(&b),label(""){}
-
-// simple get function for our class (if friendship is not used)
 template<typename T>
 T TreeNode<T>::getData(void) const { return data; }
 
-template<typename T>
-string TreeNode<T>::getLabel() { return label;}
-
 
 template<typename T>
-TreeNode<T> * TreeNode<T>::getLeftChild()
-{
-	return nextl;
-}
+TreeNode<T> * TreeNode<T>::getLeftChild(){return nextl;}
 
 template<typename T>
-TreeNode<T> * TreeNode<T>::getRightChild()
-{
-	return nextr;
-}
+TreeNode<T> * TreeNode<T>::getRightChild(){return nextr;}
 
 template<typename T>
-TreeNode<T> * TreeNode<T>::getParent()
-{
-	return parent;
-}
+TreeNode<T> * TreeNode<T>::getParent(){return parent;}
 
 template<typename T>
 string TreeNode<T>::getMostPopularResult()
