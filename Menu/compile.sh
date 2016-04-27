6e3e4d9cfd1929eb73553686232f3b9f9f3ab7b3
Luke William Duane <lduane@nd.edu>
2016-04-27 00:51:20 -0400

    compile.sh

---
+++ Menu/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+make clean
+cd ../NeuralNetwork
+make clean
+make
+cd ../MultinomialLogisticRegression
+make clean
+make
+cd ../MarkovLogicNetwork
+make clean
+make
+cd ../DecisionTree
+make clean
+make
+cd ../Menu
+make
